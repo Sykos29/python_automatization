0b3cc00a7a5e851ebfb398c138e477035535f4dd
Sykos29 <galle@gmx.net>
2024-03-03 13:04:49 +0100

    Added video downloader

---
+++ yt_download/yt_download.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ",yt.title)
+
+yd = yt.streams.get_highest_resolution();
+
+yd.download("./downloads")
